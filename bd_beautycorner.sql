808cd95505988a450163afbe6c9a58976ab37654
Jaqueline Galindo Huitron <143548375+JaquelineGalindoHuitron@users.noreply.github.com>
2023-11-09 13:18:47 -0700

    Add files via upload

---
+++ bd_beautycorner.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 02-12-2019 a las 22:14:37
+-- Versión del servidor: 5.7.28-0ubuntu0.16.04.2
+-- Versión de PHP: 7.0.33-0ubuntu0.16.04.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_beautycorner`
+--
+
+CREATE DATABASE IF NOT EXISTS `bd_beautycorner`;
+
+USE `bd_beautycorner`;
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventas`
+--
+
+CREATE TABLE IF NOT EXISTS `ventas` (
+  `idcliente` int(11) NOT NULL,
+  `idservicio` int(11) NOT NULL,
+  `idempleado` int(11) NOT NULL,
+  `direccion` varchar(75) NOT NULL,
+  `fecha` date NOT NULL,
+  `precio` int(11) NOT NULL,
+  `iva` int(11) NOT NULL,
+  `total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `ventas` (`idcliente`, `idservicio`, `idempleado`, `direccion`, `fecha`, `precio`, `iva`, `total`) 
+VALUES ("1", '524', '248', 'Revolucion 5894', '2023-11-09', '500', '120', '620'),
+("2", '525', '249', 'Revolucion 5894', '2023-11-09', '900', '200', '1100'),
+("3", '526', '250', 'Revolucion 5894', '2023-11-09', '1200', '250', '1450');
+--
+-- Indices de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD PRIMARY KEY (`idcliente`),
+  ADD UNIQUE KEY `idcliente` (`idcliente`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  MODIFY `idcliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
